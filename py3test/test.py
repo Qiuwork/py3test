c45db75abf953fceaa5405bab994582c61e5ba42
youxia <qiu0312108@163.com>
2018-11-24 14:18:37 +0800

    201811241411

--- py3test/test.py
+++ py3test/test.py
@@ -1,16 +1,16 @@
 # coding:utf-8
 
-# import os,collections
-# from operator import itemgetter
-#
-# rows = [
-# {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
-# {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
-# {'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
-# {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
-# ]
+import os,collections
+from operator import itemgetter
 
-# print(sorted(rows,key=itemgetter(-'uid')))
+rows = [
+{'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
+{'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
+{'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
+{'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
+]
+
+print(sorted(rows,key=itemgetter(-'uid')))
 
 
 
