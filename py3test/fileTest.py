264227e0fee73b046e4cc101d59f927599c5ac3d
youxia <qiu0312108@163.com>
2018-12-01 15:55:57 +0800

    20181201153803

---
+++ py3test/fileTest.py
@@ -0,0 +1,44 @@
+#_*_coding:utf-8_*_
+# 文件读取
+# 默认读取方式
+f = open('bar.py')
+ctx = f.read()
+print(ctx)
+f.close()
+# 指定读取方式
+f = open('bar.py', 'r', encoding='utf8')
+ctx = f.read()
+print(ctx)
+f.close()
+# 文件写入
+# 单次写入
+f = open('write.test', 'w', encoding='utf8')
+f.write('hello python\nhello world')
+f.close()
+# 多次写入
+f = open('write.test', 'w+', encoding='utf8')
+f.write('hello python\n')
+f.write('hello world')
+f.close()
+# 文件追加
+f = open('write.test', 'a', encoding='utf8')
+f.write('hello python\nhello world')
+f.close()
+# 二进制文件读写
+f1 = open('test.jpg', 'rb')
+f2 = open('test.bak.jpg', 'wb')
+f2.write(f1.read())
+f2.close()
+f1.close()
+# with语句块
+with open('bar.py', 'r', encoding='utf8') as f:
+    print(f.read())
+# 文件对象方法
+with open('bar.py', 'r', encoding='utf8') as f:
+    print(f.tell())
+    print(f.read())
+    print(f.tell())
+    print(f.read())
+    f.seek(0)
+    print(f.tell())
+    print(f.read())
