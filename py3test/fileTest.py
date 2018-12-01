53da90e00d5f76ff5014b89204009f763bba6455
youxia <qiu0312108@163.com>
2018-12-01 16:53:33 +0800

    20181201153804

--- py3test/fileTest.py
+++ py3test/fileTest.py
@@ -42,3 +42,80 @@ with open('bar.py', 'r', encoding='utf8') as f:
     f.seek(0)
     print(f.tell())
     print(f.read())
+
+
+str
+class Foo(object):
+    def __str__(self):
+        return "<Foo>"
+
+f = Foo()
+print(f)
+repr
+class Foo(object):
+    def __repr__(self):
+        return "<Foo repr>"
+
+f = Foo()
+print(repr(f))
+len
+class Foo(object):
+    def __len__(self):
+        return 0
+
+f = Foo()
+print(len(f))
+iter
+class Foo(object):
+    def __init__(self, name, age, sex):
+        self.set = (name, age, sex)
+        self.i = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i < len(self.set):
+            b = self.set[self.i]
+            self.i += 1
+            return b
+        else:
+            raise StopIteration()
+
+foo = Foo('python', 18, 'male')
+print(next(foo))
+for a in foo:
+    print(a)
+getitem
+class Foo(object):
+    def __init__(self, name, age, sex):
+        self.set = (name, age, sex)
+
+    def __getitem__(self, n):
+        if n < len(self.set):
+            return self.set[n]
+        else:
+            raise IndexError('index out of range')
+
+std1 = Foo('python', 18, 'female')
+print(std1[0])
+getattr
+class URL():
+    def __init__(self, path='http://api.huice.server'):
+        self._path = path
+        self.args = '?k=v'
+
+    def __getattr__(self, path):
+        return URL('%s/%s' % (self.__path, path))
+
+    def __str__(self):
+        return self._path
+
+print(URL().user.login.args)
+#练习 属性常见用的魔法属性
+
+#作用域
+- globals()
+- locals()
+- global
+- nonlocal
